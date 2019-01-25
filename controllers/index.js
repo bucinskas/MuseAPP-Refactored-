7f5f01ad3df058f784ce289c67018bee0dd251e3
Martynas <martynas@bucinskas.lt>
2019-01-25 15:22:45 +0000

    added user reister and login validations

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,38 @@
 const User = require("../models/user");
 const Shot = require("../models/shot");
 const passport = require('passport');
+const Joi = require('joi');
 
+const registerSchema = Joi.object().keys({
+  username: Joi.string()
+    .alphanum()
+    .min(7)
+    .max(30)
+    .required()
+    .error(
+      new Error(
+        'Username muse be alphanumeric and be between 7 and 30 characters'
+      )
+    ),
+    firstName: Joi.string()
+    .regex(/^[A-Za-z]+$/)
+    .min(3)
+    .max(15)
+    .required()
+    .error(
+      new Error(
+        'Your first name must contain only letters'
+      )
+    ),
+    password: Joi.string()
+    .regex(/^[a-zA-Z0-9]{3,30}$/)
+    .required()
+    .error(
+      new Error(
+        'Password cannot be empty'
+      )
+    ),
+});
 
 
 module.exports = {
@@ -12,24 +43,62 @@ module.exports = {
       res.render("login");
   },
   async postRegister(req, res, next) {
-    const newUser = new User({
-        username: req.body.username,
-        firstName: req.body.firstName,
-        email: req.body.email,
-        bio: req.body.bio,
-        location: req.body.location
+    const result = await Joi.validate(req.body, registerSchema);
+    if (result.error) {
+      req.session.error = result.error.message;
+      res.redirect('/register');
+
+    }
+
+    const userEmail = await User.findOne({
+      email: req.body.email
     });
-    await User.register(newUser, req.body.password);
-    passport.authenticate('local', {
-      successRedirect: '/',
-      failureRedirect: '/register'
-     })(req, res, next); // need to call a higher order function on passport.authenticate
+
+    if (userEmail) {
+      req.session.error = 'This email address is already in use.'
+      res.redirect('/register');
+     return;
+    }
+
+
+    const userName = await User.findOne({
+      username: req.body.username
+    });
+
+    if (userName) {
+      req.session.error = 'This username is already in use.';
+      res.redirect('/register');
+      return;
+    }
+
+    const newUser = new User({
+      username: req.body.username,
+      firstName: req.body.firstName,
+      email: req.body.email,
+      bio: req.body.bio,
+      location: req.body.location
+      });
+    
+      User.register(newUser, req.body.password);
+      passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/register',
+        failureFlash: true
+       })(req, res, next); // need to call a higher order function on passport.authenticate
   },
   postLogin(req, res, next) {
-    passport.authenticate('local', {
-     successRedirect: '/',
-     failureRedirect: '/login',
-    })(req, res, next); // need to call a higher order function on passport.authenticate
+    passport.authenticate('local', function(err, user, info) {
+      if (!user) { 
+        req.session.error = "That user does not exist"
+        res.redirect('back');
+        return;
+      }
+      req.logIn(user, function(err) {
+        if (err) { return next(err); }
+        return res.redirect('/');
+
+      });
+    })(req, res, next);// need to call a higher order function on passport.authenticate
   },
   getLogout(req, res, next) {
     req.logout();
